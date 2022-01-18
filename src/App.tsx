dbc2e7787c220f569b990fae51d79bf58bb67e96
vladislavzasyadko <gilbert.cook111@gmail.com>
2022-01-18 12:54:40 +0300

    added react-markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import './App.css'
+import ReactMarkdown from "react-markdown";
+
+import remarkGfm from "remark-gfm";
 
 function App() {
+    const markdown: string = `
+  # Header 1
+  ## Header 2
 
-  return (
-    <div className={'main'}>
-      Organizer
-    </div>
-  )
+  _italic_
+
+  **bold**
+  
+  ~~strikethrough~~
+  
+  * [ ] task
+  
+  * [x] task
+  `;
+
+    return (
+        <div className={'main'}>
+            Organizer
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                {markdown}
+            </ReactMarkdown>
+        </div>
+    )
 }
 
 export default App
